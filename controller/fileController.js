85b6395802e75ba30fe765d8fb19afe8d2f5ee41
Sarthak Bhatt <sarthak@MacBook-Air-2.local>
2025-09-02 15:04:55 +0530

    add

--- controller/fileController.js
+++ controller/fileController.js
@@ -1,9 +1,17 @@
+const path = require("path");
+const fs = require("fs");
+
 const downloadFile = async (req, res) => {
-  const { filePath } = req.query;
+  const { filePath, title } = req.query;
 
-  const file = `${__dirname}/../${filePath}`;
+  const file = path.join(__dirname, "..", filePath);
 
-  res.download(file);
+  // Use title from params, fallback to original filename if not provided
+  const saveName = title
+    ? `${title}${path.extname(file)}`
+    : path.basename(file);
+
+  res.download(file, saveName);
 };
 
 exports.downloadFile = downloadFile;
